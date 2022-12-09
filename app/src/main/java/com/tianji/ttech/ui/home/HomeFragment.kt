bd4e881cb9b096b8981c62685ea3ddd0b82ff025
grt-liaojinsha <721695214@qq.com>
2022-12-09 19:07:35 +0800

    首页开发

--- app/src/main/java/com/tianji/ttech/ui/home/HomeFragment.kt
+++ app/src/main/java/com/tianji/ttech/ui/home/HomeFragment.kt
@@ -1,21 +1,29 @@
 package com.tianji.ttech.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.OnClickListener
 import android.view.ViewGroup
 import androidx.activity.viewModels
+import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import com.tianji.ttech.R
 import com.tianji.ttech.base.BaseFragment
 import com.tianji.ttech.databinding.FragmentHomeBinding
+import com.tianji.ttech.model.StationModel
 import com.tianji.ttech.ui.common.viewmodel.SelectAreaViewModel
+import com.tianji.ttech.ui.home.storage.HomeStatusFragment
 import com.tianji.ttech.ui.station.StationViewModel
 import com.tianji.ttech.view.dialog.OptionsDialog
 import com.tianji.ttech.view.pop.ListPopuwindow
 import com.tianji.ttech.view.popuwindow.ListPopModel
+import com.ttech.lib.util.LogUtil
 import com.ttech.lib.util.ToastUtil
+import com.ttech.lib.util.gone
+import com.ttech.lib.util.visible
 
 class HomeFragment : BaseFragment(), OnClickListener {
 
@@ -25,11 +33,25 @@ class HomeFragment : BaseFragment(), OnClickListener {
 
     private val viewModel: StationViewModel by viewModels()
 
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        LogUtil.i("homestatusFragment", "HomeFragment onAttach")
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        LogUtil.i("homestatusFragment", "HomeFragment onCreate")
+    }
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        LogUtil.i("homestatusFragment", "HomeFragment onCreateView")
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         initData()
         setliseners()
@@ -47,10 +69,21 @@ class HomeFragment : BaseFragment(), OnClickListener {
 
     private fun initData() {
         viewModel.getPlantListLiveData.observe(viewLifecycleOwner) {
+            _binding.srlRefresh.finishRefresh()
             if (it.first) {
                 val second = it.second
                 if (second == null || second.isEmpty()) {//没有电站  显示空
                     _binding.header.tvTitle.text = ""
+                    _binding.emptyView.root.visible()
+                    _binding.fragmentSystem.gone()
+                }else {
+                    _binding.emptyView.root.gone()
+                    _binding.fragmentSystem.visible()
+
+                    //默认选中第一个电站
+                    _binding.header.tvTitle.text = second[0].stationName
+                    //显示系统图
+                    showSystemSatus(second[0])
 
                 }
             }
@@ -59,15 +92,28 @@ class HomeFragment : BaseFragment(), OnClickListener {
     }
 
 
+
+    private fun showSystemSatus(station: StationModel){
+        val stationType = station.stationType
+        //根据电站类型显示不同界面
+        childFragmentManager.commit(true) {
+            if ("PV Station" == stationType) {//纯光伏电站
+                add(R.id.fragment_system, HomeStatusFragment())
+            } else {
+
+            }
+        }
+
+    }
+
+
+
     private fun fetchPlantList() {
         showDialog()
         viewModel.getPlantList()
     }
 
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-    }
 
     override fun onClick(p0: View?) {
         when {
@@ -92,6 +138,47 @@ class HomeFragment : BaseFragment(), OnClickListener {
             }
             context?.let { ListPopuwindow.showPop(it, options, _binding.header.tvTitle) }
         }
+    }
+
+
+
+    override fun onStart() {
+        super.onStart()
+        LogUtil.i("homestatusFragment", "HomeFragment onStart")
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        LogUtil.i("homestatusFragment", "HomeFragment onResume")
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        LogUtil.i("homestatusFragment", "HomeFragment onPause")
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        LogUtil.i("homestatusFragment", "HomeFragment onStop")
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        LogUtil.i("homestatusFragment", "HomeFragment onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        LogUtil.i("homestatusFragment", "HomeFragment onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        LogUtil.i("homestatusFragment", "HomeFragment onDetach")
     }
 
 
--- app/src/main/java/com/tianji/ttech/ui/home/storage/HomeStatusFragment.kt
+++ app/src/main/java/com/tianji/ttech/ui/home/storage/HomeStatusFragment.kt
@@ -1,5 +1,6 @@
 package com.tianji.ttech.ui.home.storage
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,11 +8,24 @@ import android.view.ViewGroup
 import com.tianji.ttech.base.BaseFragment
 import com.tianji.ttech.databinding.FragmentSystemStatusBinding
 import com.tianji.ttech.model.StationModel
+import com.ttech.lib.util.LogUtil
 
-class HomeStatusFragment(val station: StationModel) : BaseFragment() {
+class HomeStatusFragment() : BaseFragment() {
 
 
     private lateinit var _binding: FragmentSystemStatusBinding
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        LogUtil.i("homestatusFragment", "homestatusFragment onAttach")
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        LogUtil.i("homestatusFragment", "homestatusFragment onCreate")
+    }
 
 
     override fun onCreateView(
@@ -19,8 +33,52 @@ class HomeStatusFragment(val station: StationModel) : BaseFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        LogUtil.i("homestatusFragment", "homestatusFragment onCreateView")
         _binding = FragmentSystemStatusBinding.inflate(inflater, container, false)
         return _binding.root
     }
+
+
+
+
+    override fun onStart() {
+        super.onStart()
+        LogUtil.i("homestatusFragment", "homestatusFragment onStart")
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        LogUtil.i("homestatusFragment", "homestatusFragment onResume")
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        LogUtil.i("homestatusFragment", "homestatusFragment onPause")
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        LogUtil.i("homestatusFragment", "homestatusFragment onStop")
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        LogUtil.i("homestatusFragment", "homestatusFragment onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        LogUtil.i("homestatusFragment", "homestatusFragment onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        LogUtil.i("homestatusFragment", "homestatusFragment onDetach")
+    }
+
 
 }
