ba4ff82dd538bd8e8e5463d724c6bc53814262e7
grt-liaojinsha <721695214@qq.com>
2022-12-17 12:13:47 +0800

    退出登录功能

--- app/src/main/java/com/tianji/ttech/service/account/DefaultAccountService.kt
+++ app/src/main/java/com/tianji/ttech/service/account/DefaultAccountService.kt
@@ -3,10 +3,12 @@ package com.tianji.ttech.service.account
 import android.content.Context
 import com.ttech.lib.service.account.BaseAccountService
 import com.tianji.ttech.app.Foreground
+import com.tianji.ttech.ui.account.login.activity.LoginActivity
 
 class DefaultAccountService : BaseAccountService() {
 
     override fun login(context: Context) {
+        LoginActivity.startClearTask(context)
     }
 
     override fun tokenExpired() {
--- app/src/main/java/com/tianji/ttech/ui/manu/ManuFragment.kt
+++ app/src/main/java/com/tianji/ttech/ui/manu/ManuFragment.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.tianji.ttech.base.BaseFragment
 import com.tianji.ttech.databinding.FragmentHomeMineBinding
 import com.tianji.ttech.ui.manu.activity.SettingActivity
+import com.tianji.ttech.ui.manu.viewmodel.ManuViewModel
+import com.tianji.ttech.ui.manu.viewmodel.SettingViewModel
+import com.ttech.lib.util.ToastUtil
 
 class ManuFragment : BaseFragment(), View.OnClickListener {
 
     private var _binding: FragmentHomeMineBinding? = null
 
     private val binding get() = _binding!!
+
+
+    private val viewModel: ManuViewModel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,16 +36,26 @@ class ManuFragment : BaseFragment(), View.OnClickListener {
 
     private fun setListeners() {
         binding.llAccount.setOnClickListener(this)
+        binding.btnLogout.setOnClickListener(this)
     }
-
 
 
     private fun initData() {
         val email = accountService().user()?.email
         binding.tvUserName.text = email
+
+
+        viewModel.logoutLiveData.observe(viewLifecycleOwner) {
+            dismissDialog()
+            if (it == null) {
+                accountService().logout()
+                accountService().login(requireActivity())
+            } else {
+                showResultDialog(it,null,null)
+            }
+        }
+
     }
-
-
 
 
     override fun onDestroyView() {
@@ -48,6 +66,11 @@ class ManuFragment : BaseFragment(), View.OnClickListener {
     override fun onClick(p0: View?) {
         when {
             p0 === binding.llAccount -> SettingActivity.start(context)
+            p0 === binding.btnLogout -> {
+                showDialog()
+                viewModel.logout(accountService().user()?.email)
+
+            }
 //            p0 === binding.itemAbout -> AboutActivity.start(context)
 //            p0 === binding.itemMessageCenter -> MessageCenterActivity.start(context)
         }
